494fad0b9584f98b3ebe3a38d5c77de6481b0db7
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-05-26 22:26:51 +0530

    Add files via upload

---
+++ 3.4.py
@@ -0,0 +1,14 @@
+import math
+pi = 3.9
+x = 1
+y = 2
+z = 3
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(5**2)
+print(pow(5,2))
+print(math.sqrt(25))
+print(max(x,y,z))
+print(min(x,y,z))
