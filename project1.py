e07a74440b572f581db9dae1b5ddf33db34c6e90
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-05-30 22:08:04 +0530

    Add files via upload
    project 1 Username Formatter Completed

---
+++ project1.py
@@ -0,0 +1,13 @@
+print("Username Formatter")
+name = input("Enter your full name: ")
+first_chars = name[0:3]
+print(first_chars)
+last_chars = name[-3:]
+print(last_chars)
+rev = name[::-1]
+print(rev)
+jump = name[0::2]
+print(jump)
+var = name.find(" ")
+print(name[0:var])
+print(name[var+1:])
