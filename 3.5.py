c31fd1987c9cd157567dc4ce89889357e990c6f4
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-05-30 14:15:33 +0530

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,13 @@
+name = "Chota Bheem"
+title = "python"
+first_name = name[:5]
+print(first_name)
+last_name = name[6:]
+print(last_name)
+title_name = title[0:6:-1]
+print(title_name)
+website = "http://google.com"
+lice = slice(7,13)
+print(website[lice])
+
+
