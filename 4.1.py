8019008d94504fb0906c067ac5c02188a322a4e1
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-01 23:04:28 +0530

    Add files via upload
    Project 2: Student Grade Checker Completed

---
+++ 4.1.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you: "))
+if age == 100:
+    print("you are a centuary old")
+elif age >=18:
+    print("you are an adult ")
+elif age <0:
+    print("you haven't been born yet")    
+else:
+    print("you are a child")
+
---
+++ project2.py
@@ -0,0 +1,17 @@
+print("Project2:Student Grade Checker") 
+marks = int(input("Enter you Marks: "))  
+if marks>=90 and marks<=100:
+    print("Grade A")             
+elif marks>=80 and marks<=89:
+    print("Grade B")
+elif marks>=70 and marks<=79:
+    print("Grade C")
+elif marks>=60 and marks<=69:
+    print("Grade D")
+elif marks>=50 and  marks<=59:
+    print("Grade E")  
+else:
+    print("Fail")    
+    
+    
+
