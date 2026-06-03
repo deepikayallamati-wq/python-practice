3d1a0fb9dd17fea5a19459339b071a7f5cc2d174
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-03 22:32:07 +0530

    Add files via upload

---
+++ 6.1.py
@@ -0,0 +1,4 @@
+name = None
+while not name:       
+    name = input("Enter your name:")
+print("Hello "+name)
---
+++ 6.2.py
@@ -0,0 +1,17 @@
+import time
+for i in range(10):
+    print(i)
+
+for i in range(91,100+1,2):
+     print(i)
+
+for i in "Ding Dong":
+    print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year")
+
+
+
