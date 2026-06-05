ced37f5422201d0ec00fd30e72bbafc44fad607b
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-05 14:24:02 +0530

    Add files via upload

---
+++ 7.2.py
@@ -0,0 +1,22 @@
+while True:
+    name = input("Enter your name: ")
+    if name != "":
+         break
+
+phone_number = "123_456_7890"
+for i in phone_number:
+     if i =="_":
+          continue
+     print(i,end="")
+
+for i in range(0,11):
+    if i == 6:
+        pass
+    
+        
+    
+    
+         
+   
+             
+ 
