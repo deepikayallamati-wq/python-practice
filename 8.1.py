cd57bdbb6333ed81dd5811f969f081ae770fb6f4
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-09 14:33:49 +0530

    Add files via upload

---
+++ 8.1.py
@@ -0,0 +1,12 @@
+food = ["laddu","rasgula","malipuri","mysorepak"]
+food[2] = "gilabi"
+print(food[2])
+food.append("cream")
+food.remove("laddu")
+food.insert(3,"badham milk")
+food.pop(1)
+food.sort()
+#food.clear()
+
+for x in food:
+    print(x)
