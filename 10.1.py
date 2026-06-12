79d18eefb3840bddc0b5ed7c10f5b5befae79a1f
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-12 11:25:13 +0530

    Add files via upload

---
+++ 10.1.py
@@ -0,0 +1,11 @@
+name = "heidi adilade"
+if(name[0].islower()):
+   name = name.capitalize()
+   print(name)
+first_name = name[:5].upper()
+last_name = name[6:].lower()
+last_character = name[-2] 
+print(first_name)
+print(last_name)
+print(last_character)
+   
