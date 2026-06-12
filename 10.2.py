f17ddca9331f384517a591556134b7525fb932c1
DEEPIKA YALLAMATI <deepikayallamati@gmail.com>
2026-06-12 12:55:23 +0530

    Add files via upload

---
+++ 10.2.py
@@ -0,0 +1,7 @@
+def hello(first_name,last_name,age):
+    print("Hello "+ first_name+" "+last_name)
+    print("You are "+str(age)+" Years old")
+    print("Have a niceday")
+hello("Donald","Duck",21)
+
+   
